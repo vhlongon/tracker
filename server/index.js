632f9e59a21c84dc56de676fb795bdd387269696
Victor Longon <victor.longon@etraveligroup.com>
2020-07-14 18:46:28 +0200

    setup test route for signup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/authRoutes");
 const { password } = require("./secret");
 
 const app = express();
-const mongoUri = `mongodb+srv://admin:${password}@cluster0.6n64k.mongodb.net/tracker?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useCreateIndex: true });
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = `mongodb+srv://admin:${password}@cluster0.6n64k.mongodb.net/tracker?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 mongoose.connection.on("connected", () => {
   console.log("Connected to mongo instance");
