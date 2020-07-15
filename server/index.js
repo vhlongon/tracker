cf4fee54b85b7bc746a4653a97a72245ebb98afc
Victor Longon <victor.longon@etraveligroup.com>
2020-07-15 15:04:46 +0200

    fix some linting in  server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 // only requires once otherwise the model is recreated if imported several times and mongoose will throw an error
-require("./models/User");
-require("./models/Track");
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const authRoutes = require("./routes/authRoutes");
-const trackRoutes = require("./routes/trackRoutes");
-const requireAuth = require("./middlewares/requireAuth");
-const { password } = require("./secret");
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+const { password } = require('./secret');
 
 const app = express();
 
@@ -22,19 +22,19 @@ mongoose.connect(mongoUri, {
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on("connected", () => {
-  console.log("Connected to mongo instance");
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
 });
 
-mongoose.connection.on("error", error => {
-  console.error("Error Connected to mongo", error);
+mongoose.connection.on('error', error => {
+  console.error('Error Connected to mongo', error);
 });
 
 // use the middleware to check if the user in loggedin
-app.get("/", requireAuth, (req, res) => {
+app.get('/', requireAuth, (req, res) => {
   res.send(`your email ${req.user.email}`);
 });
 
 app.listen(3000, () => {
-  console.log("listening on port 3000");
+  console.log('listening on port 3000');
 });
